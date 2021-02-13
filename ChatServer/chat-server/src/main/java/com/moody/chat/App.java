7da1425d759b3e1574fdb82d5794538985bc64ee
erikmmoody <erikmmoody@gmail.com>
2021-02-13 16:48:39 -0600

    Initialize project

---
+++ ChatServer/chat-server/src/main/java/com/moody/chat/App.java
@@ -0,0 +1,20 @@
+package com.moody.chat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        List<String> strings = new ArrayList<>();
+        strings.add("one");
+        strings.add("two");
+        strings.add("three");
+    }
+}
